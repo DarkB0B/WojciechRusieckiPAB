6fb974f1bca28333e1f52792a387f73625131ed0
DarkB0B <miecze123@onet.pl>
2022-03-23 18:25:16 +0100

    nic nie działa jest super (lab3)

--- Projekt2/index.ts
+++ Projekt2/index.ts
@@ -1,23 +1,69 @@
+import { notStrictEqual } from 'assert'
+import { Console } from 'console'
 import express from 'express'
-import {Request, Response} from 'express'
-import {Note} from './note'
+import { Request, Response } from 'express'
+import { normalize } from 'path'
+import { Note } from './note'
 
 let notes: Note[] = []
 //let note = notes.find( note => note.id == 10)
 const app = express()
+const date = new Date()
+const testNote: Note = new Note
+   ({
+    title : 'TestTitle',
+    content : 'TestContent',
+    createDate : date.toISOString(),
+    tags : [],
+    id : Date.now()
+  })
 
 app.use(express.json())
 
-app.get('/:id', function (req: Request, res: Response) {
-  const note = {title: 'asd'}
-  res.send(note)
+app.get('/note/:id', function (req: Request, res: Response) { //odczyt notatki
+  var thisNoteId: number = +req.params.id
+  let note = notes.find(note => note.id == thisNoteId)
+  note ?? res.send(404)
+  res.status(200).send(note)
 })
-app.post('/', function (req: Request, res: Response) {
- // console.log(req.body) // e.x. req.body.title 
- let lastnote: Note = notes.slice(-1)[0]
- let x: number = lastnote.id + 1
- let thisnote: Note = new Note({title : req.body.title, content : req.body.content, createDate : req.body.createDate, tags : req.body.tags,id : x})
-  res.status(200).send('asd')
+app.post('/note', function (req: Request, res: Response) { //dodanie nowej notatki
+ if (req.body.title && req.body.content)
+ {
+  
+  const date = new Date()
+  const thisnote: Note = new Note
+   ({
+    title : req.body.title,
+    content : req.body.content,
+    createDate : date.toISOString(),
+    tags : req.body.tags,
+    id : Date.now()
+  })
+  notes.push(thisnote)
+  res.status(200).send(thisnote)
+}
+else
+{
+  res.status(400).send("Wymagane pola notatki są puste")
+}
+  
+})
+app.put('/note/:id', function (req: Request, res: Response)
+{
+  var thisNoteId: number = +req.params.id
+  let note = notes.find(note => note.id == thisNoteId)
+  note.title = 'ChangedTitle'
+  note ?? res.status(404)
+  res.status(204)
+})
+app.delete('/note/:id', function (req: Request, res: Response)
+{
+  var thisNoteId: number = +req.params.id
+  let note = notes.find(note => note.id == thisNoteId)
+  const index = notes.findIndex(object => {return object.id === note.id})
+  notes.splice(index,1)
+  note ?? res.status(400).send('Notatka nie istnieje')
+  res.status(204)
 })
 
 app.listen(3000)
